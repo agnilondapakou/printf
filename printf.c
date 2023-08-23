b4106c67122b5aceb2435f291dea1196d86317de
Yannick <pagnilonda@gmail.com>
2023-08-23 00:11:46 +0000

    handle d and i formats

--- printf.c
+++ printf.c
@@ -24,12 +24,17 @@ int _printf(const char *format, ...)
 				count += _putchar(va_arg(args, int));
 			else if (format[i] == 's')
 				count += print_str(va_arg(args, char *), j);
-			else if (format[i] == '\0')
-				return (-1);
 			else if (format[i] == 'd' || format[i] == 'i')
 				count += print_int(va_arg(args, int));
+			else if (format[i] == '\0')
+				return (-1);
+			else if (format[i] == '%')
+				count += _putchar('%');
 			else
+			{
+				count += _putchar('%');
 				count += _putchar(format[i]);
+			}
 		}
 		else
 			count += _putchar(format[i]);
